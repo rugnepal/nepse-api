5100f11676ba7fca6852a1d3519ded63b3efbcec
bjungbogati <8600091+bjungbogati@users.noreply.github.com>
2022-06-19 18:27:49 +0545

    changed numbers to characters

--- scrape_nepse_data.R
+++ scrape_nepse_data.R
@@ -1,8 +1,8 @@
-library(rvest)
-library(janitor)
-library(dplyr)
-library(readr)
-library(jsonlite)
+library(rvest, include.only = c("read_html", "html_nodes", "html_table"))
+library(janitor, include.only = "clean_names")
+library(dplyr, include.only = c("mutate", "bind_rows", "distinct"))
+library(readr, include.only = "write_csv")
+library(jsonlite, include.only = "write_json")
 
 url <- "https://www.sharesansar.com/today-share-price"
 rds_path <- "./data/nepse-daily-data-2079.rds"
@@ -14,16 +14,19 @@ share_tbl <- read_html(url) %>%
   html_table() %>%
   data.frame() %>%
   clean_names() %>%
-  mutate(date = Sys.Date())
+  mutate(
+    across(everything(), as.character),
+    date = Sys.Date()
+  )
 
 save_files <- \() {
   rds <- readRDS(rds_path) %>%
     bind_rows(share_tbl) %>%
     distinct(symbol, date, .keep_all = T)
-  
-    saveRDS(rds, rds_path)
-    write_csv(rds, csv_path) # toJSON(x = rds, dataframe = 'rows', pretty = T)
-    write_json(rds, json_path)
+
+  saveRDS(rds, rds_path)
+  write_csv(rds, csv_path) # toJSON(x = rds, dataframe = 'rows', pretty = T)
+  write_json(rds, json_path)
 }
 
 
